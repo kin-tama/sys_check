20a0ceaa277ef8b5f8a513cf6d1c146b1893f78f
Roman Kavandin <kavandin@yandex-team.ru>
2021-09-15 19:56:24 +0300

    job is done

---
+++ build/script.js
@@ -0,0 +1,42 @@
+var parser = new UAParser();
+
+
+var browser = parser.getBrowser();
+var cpu = parser.getCPU();
+var os = parser.getOS();
+var device = parser.getDevice();
+var engine = parser.getEngine();
+var width = window.screen.width;
+var height = window.screen.height;
+var depth = window.screen.colorDepth;
+var orientation = window.screen.orientation;
+var viewportHeight = window.visualViewport.height
+var viewportWidth = window.visualViewport.width
+
+var tabCPU = document.getElementById('CPU');
+var tabOS = document.getElementById('OS');
+var tabDevice = document.getElementById('dev');
+var tabBrowser = document.getElementById('browser');
+var tabWidth = document.getElementById('width');
+var tabHeight = document.getElementById('heigth');
+var tabOrientation = document.getElementById('orientation');
+var tabDepth = document.getElementById('depth');
+var tabAgent = document.getElementById('agent');
+var main = document.getElementById('main');
+
+tabCPU.textContent = cpu.architecture ? cpu.architecture : "unavailable";
+tabOS.textContent = `${os.name}, version: ${os.version}`;
+tabDevice.textContent = `type: ${device.type ?? 'unavailable'}, model: ${device.model ?? 'unavailable'}, vendor: ${device.vendor ?? 'unavailable'}`;
+tabBrowser.textContent = `Name: ${browser.name ?? 'unavailable'}, major: ${browser.major ?? 'unavailable'}, version: ${browser.version ?? 'unavailable'}`;
+tabWidth.textContent = `${width ?? 'unavailable'} pixels`;
+tabHeight.textContent = `${height ?? 'unavailable'} pixels`;
+tabOrientation.textContent = `${orientation.type ?? 'unavailable'}`;
+tabDepth.textContent = `${depth ?? 'unavailable'} bit`;
+tabAgent.textContent = window.navigator.userAgent;
+main.style.width = viewportWidth;
+main.style.height = viewportHeight;
+
+if (window.navigator.userAgent.toLowerCase().indexOf('intel')) {
+    tabCPU.textContent = 'Intel'
+}
+
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require("gulp");
+const sync = require("browser-sync").create();
+const del = require("del");
+
+
+
+const server = (done) => {
+    sync.init({
+        server: {
+            baseDir: 'build'
+        },
+        cors: true,
+        notify: false,
+        ui: false,
+    });
+    done();
+}
+
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+    gulp.watch("source/*.css", gulp.series("build")).on("change", sync.reload);
+    gulp.watch("source/*.html", gulp.series("build")).on("change", sync.reload);
+    gulp.watch("source/*.js", gulp.series("build")).on("change", sync.reload);
+}
+
+exports.default = gulp.series(
+    server, watcher
+);
+
+// copy
+
+const copy = () => {
+    return gulp.src([
+        "source/*.js",
+        "source/*.css",
+        "source/*.html"
+    ], {
+        base: "source"
+    })
+        .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
+
+
+// delete
+
+const clean = () => {
+    return del("build");
+};
+
+exports.clean = clean;
+
+// build
+
+const build = gulp.series(
+    clean,
+    copy,
+);
+
+exports.build = build;
+
+// start
+
+const start = gulp.series(
+    build,
+    server,
+    watcher,
+);
+
+exports.start = start;
